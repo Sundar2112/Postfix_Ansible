9fc9daa7b33934eff315a99462f30a681f8a57d2
Sundar2112 <101797408+Sundar2112@users.noreply.github.com>
2022-03-17 15:16:51 +0530

    Add files via upload

---
+++ files/sch.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS `virtual_domains` (
+    `id`  INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `virtual_users` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `domain_id` INT NOT NULL,
+    `password` VARCHAR(106) NOT NULL,
+    `email` VARCHAR(120) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `email` (`email`),
+    FOREIGN KEY (domain_id) REFERENCES virtual_domains(id) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
